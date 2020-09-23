4158aa376f21cedaf432d6b600e8107ba4554c49
Victor Porton <porton@narod.ru>
2020-09-23 18:06:49 +0300

    buidler.config.js

--- buidler.config.js
+++ buidler.config.js
@@ -25,11 +25,28 @@ module.exports = {
     },
   },
   networks: {
+    buidlerevm: {
+      accounts: [
+        {
+          privateKey: '0x0e206566a53a138f9500dd3ffaf12bbf3c773a34a0e78e6710b0726b82951e6d', // 0xfd95BF6727416050008dB2551c94C86D21bA3b77
+          balance: '0xf0000000000000000',
+        },
+        {
+          privateKey: '0x3d258b188e1e2bd69821990cc143830ce2be03dc24774c787090de8ef6bca214', // 0x4948C09461d37946Ea13b98d2C3f2D3C185fde2f
+          balance: '0xf0000000000000000',
+        },
+      ],
+    },
+    ganache: {
+      gasLimit: 6000000000,
+      defaultBalanceEther: 10,
+      url: "http://localhost:8545",
+    },
     rinkeby: {
       url: "https://rinkeby.infura.io/v3/1d0c278301fc40f3a8f40f25ae3bd328",
       accounts: process.env.RINKEBY_PRIVATE_KEY ? [process.env.RINKEBY_PRIVATE_KEY] : [],
     },
-      mainnet: {
+    mainnet: {
       url: "https://mainnet.infura.io/v3/1d0c278301fc40f3a8f40f25ae3bd328",
       accounts: process.env.MAINNET_PRIVATE_KEY ? [process.env.MAINNET_PRIVATE_KEY] : [],
     },
