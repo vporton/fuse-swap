c08995b841a560576953e9797a6f4152c26c5f97
Victor Porton <porton@narod.ru>
2020-09-24 02:32:09 +0300

    deployed

--- ui/js/addresses.js
+++ ui/js/addresses.js
@@ -9,4 +9,4 @@ const fuseToken = '0x970B9bB2C0444F5E81e9d0eFb84C8ccdcdcAf84d';
 
 const uniswapV2Router02Address = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 
-const mySwapAddress = '';
+const mySwapAddress = '0x3ed8DA2E5185FEc2FB0A275Ff8b46e1167F44035';
